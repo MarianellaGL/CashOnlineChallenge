7addf3ce535de55f5847af471c3745053c5bc763
MarianellaGL <marianella.gomezluna@gmail.com>
2019-11-27 20:31:32 -0300

    Primer commit del challenge de cash online para la API

---
+++ backendchallenge/src/test/java/ar/com/api/cashonline/backendchallenge/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.api.cashonline.backendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
